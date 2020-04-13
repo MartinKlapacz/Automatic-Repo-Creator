acca4df631171e673104e596edae3658345618d6
Martin Klapacz <klapacz.martin@gmail.com>
2020-04-13 23:40:17 +0200

    updated setup.sh

--- setup.sh
+++ setup.sh
@@ -3,13 +3,11 @@
 # python, package manager and package installation
 sudo apt install python
 sudo apt install python-pip
-pip install selenium
+pip install selenium --user
 
 # make file executable
 chmod +x createrepo
 
-# move files 
+# move main script and chromedriver executable to $PATH location
 sudo cp createrepo /usr/local/bin
 sudo cp chromedriver /usr/local/bin
-# export PATH=$PATH:/usr/local/bin/chromedriver
-# source ~/.profile 
